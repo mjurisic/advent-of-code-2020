c17fe2c00eb1128e48add7eb9770ce20af56950d
Marko Jurisic <marko.jurisic@willhaben.at>
2020-12-04 15:26:37 +0100

    day1-1

---
+++ src/org/mjurisic/aoc2020/Day1.kt
@@ -0,0 +1,31 @@
+package org.mjurisic.aoc2020
+
+import java.io.File
+import java.util.*
+
+class Day1 {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            try {
+                var entries = ArrayList<Int>()
+                File("/home/jurisima/playground/aoc2020/src/org/mjurisic/aoc2020/input1-1.txt").forEachLine {
+                    entries.add(Integer.parseInt(it))
+                }
+
+                for (i in (0 until entries.size)) {
+                    for (j in (i + 1 until entries.size - 1)) {
+                        if (entries[i] + entries[j] == 2020) {
+                            println(entries[i] * entries[j])
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+}
