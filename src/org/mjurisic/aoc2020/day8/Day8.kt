2e2198ec4e0d3b3fc327bb9b481ef0514cad4be7
Marko Jurisic <marko.jurisic@willhaben.at>
2020-12-08 13:24:46 +0100

    day8-2

--- src/org/mjurisic/aoc2020/day8/Day8.kt
+++ src/org/mjurisic/aoc2020/day8/Day8.kt
@@ -8,8 +8,11 @@ class Day8 {
         @JvmStatic
         fun main(args: Array<String>) = try {
             val commands = ArrayList<Command>()
-            var acc = 0
-            var i = 0
+
+            var isDone = false
+            var changedIndex = 0
+            var previousChangedIndex = -1
+            var acc:Int
 
             File(ClassLoader.getSystemResource("resources/input8.txt").file).forEachLine {
                 val command = it.substring(0, it.indexOf(' '))
@@ -20,23 +23,66 @@ class Day8 {
                 commands.add(Command(command, value.toInt()))
             }
 
-            while(true) {
-                val command = commands[i]
-                if (command.isDone) {
-                    break
-                }
-                when (command.command) {
-                    "nop" -> i++
-                    "acc" -> {
-                        acc += command.value
-                        i++
-                    }
-                    "jmp" -> i += command.value
-                }
-                command.isDone = true
-            }
+            do {
+                acc = 0
+                var i = 0
 
-            println(acc)
+                while (true) {
+                    if (i == commands.size -1) {
+                        isDone = true
+                        break
+                    }
+                    val command = commands[i]
+                    if (command.isDone) {
+                        break
+                    }
+                    when (command.command) {
+                        "nop" -> i++
+                        "acc" -> {
+                            acc += command.value
+                            i++
+                        }
+                        "jmp" -> i += command.value
+                    }
+
+                    if (command.isDone && changedIndex == previousChangedIndex) {
+                        break
+                    }
+                    command.isDone = true
+                    previousChangedIndex = changedIndex
+                }
+
+                // reset command state between loops
+                commands.forEach{
+                    it.isDone = false
+                    if (it.originalCommand != null) {
+                        it.command = it.originalCommand!!
+                    }
+                }
+
+                // mutate commands to break the loop
+                var found = false
+                while(!found && changedIndex < commands.size) {
+                    val command = commands[changedIndex]
+                    if (command.command == "jmp") {
+                        found = true
+                        command.command = "nop"
+                        command.originalCommand = "jmp"
+                    } else if (command.command == "nop") {
+                        found = true
+                        command.command = "jmp"
+                        command.originalCommand = "nop"
+                    }
+                    changedIndex++
+
+                }
+
+            } while (!isDone && i < commands.size )
+
+
+
+            println("acc:$acc")
+
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -45,6 +91,7 @@ class Day8 {
 
 class Command(var command: String, var value: Int) {
     var isDone = false
+    var originalCommand:String? = null
     override fun toString(): String {
         return "Command(command='$command', value=$value)"
     }
