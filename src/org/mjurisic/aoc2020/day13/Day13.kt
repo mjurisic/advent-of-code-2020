caeeae8d19c3dcc409a6d82b58eb5e848eab50a7
Marko Jurisic <marko.jurisic@willhaben.at>
2020-12-14 18:51:44 +0100

    day13

--- src/org/mjurisic/aoc2020/day13/Day13.kt
+++ src/org/mjurisic/aoc2020/day13/Day13.kt
@@ -25,38 +25,35 @@ class Day13 {
             println("$min, $index, ${numbers[index] * min}")
 
 
-            //part 2
-            val ex1 = "67,x,7,59,61"
-            val n = ex1.replace('x', '1').split(",").filter { it != "x" }.map { it.toLong() }
-            println(n)
-            var start: Long = 0
-            var found = false
-            while (!found) {
-                found = true
-                if (start % 1000000000L == 0L) {
-                    println(start)
-                }
-                for (i in start until start + n.size) {
+            //part 2, stolen from https://github.com/p88h/aoc2020/blob/main/src/main/kotlin/day13.kt
+            val ex1 = "13,x,x,41,x,x,x,37,x,x,x,x,x,419,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,23,x,x,x,x,x,29,x,421,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,17"
+            val sched = ex1.split(',').mapIndexed { p, v -> v to p }
+                .filter { it.first != "x" }.map { it.first.toLong() to it.second }.toList()
+            val start = 1002394L
 
-                    val index = (i - start).toInt()
-                    if (i % n[index] != 0L) {
-                        found = false
-                        break
-//                        println ("$i, ${n[i - start]}")
-                    }
-                }
-                if (found) {
-                    break
-                } else {
-                    start++
-                }
-            }
 
-            println(start)
+            val prod = sched.map { it.first }.reduce { a, b -> a * b }
+            println(sched.map {
+                val rem = (it.first - it.second) % it.first
+                val pp = prod / it.first
+                ( rem * inv(pp, it.first) * pp )
+            }.sum() % prod)
+
+
 
 
         } catch (e: Exception) {
             e.printStackTrace()
+        }
+
+        internal fun inv(a0: Long, m0: Long): Long {
+            var m = m0; var a = a0; var x0 = 0L; var x1 = 1L
+            while (a > 1) {
+                val q = a / m;
+                val t0 = m; m = a % m; a = t0
+                val t1 = x0; x0 = x1 - q * x0; x1 = t1
+            }
+            return if (x1 < 0) x1 + m0 else x1
         }
 
 
