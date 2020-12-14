30bd5ce70cda6b06baaa68f5bba4ddb629ec35f2
Marko Jurisic <marko.jurisic@willhaben.at>
2020-12-14 19:48:02 +0100

    day14, cleanup

--- src/org/mjurisic/aoc2020/day14/Day14.kt
+++ src/org/mjurisic/aoc2020/day14/Day14.kt
@@ -9,20 +9,18 @@ class Day14 {
         @JvmStatic
         fun main(args: Array<String>) = try {
 
-            var mask = ""
-            var mem = HashMap<String, BigInteger>()
+            var mask= ""
+            val mem = HashMap<String, BigInteger>()
 
             File(ClassLoader.getSystemResource("resources/input14.txt").file).forEachLine {
                 if (it.startsWith("mask")) {
                     mask = it.substring(7)
                 } else {
-                    val groups = Regex("mem\\[(\\d+)\\] = (\\d+)").find(it)!!.groups
-                    var address = groups[1]!!.value
-                    var value = BigInteger(groups[2]!!.value)
+                    val groups = Regex("mem\\[(\\d+)] = (\\d+)").find(it)!!.groups
+                    val address = groups[1]!!.value
+                    val value = BigInteger(groups[2]!!.value)
 
                     mem[address] = calculateValue(mask, value)
-
-
                 }
 
             }
@@ -41,7 +39,7 @@ class Day14 {
 
         private fun calculateValue(mask: String, value: BigInteger): BigInteger {
             var result = ""
-            var stringValue = value.toString(2).reversed()
+            val stringValue = value.toString(2).reversed()
             mask.reversed().forEachIndexed { i, c ->
                 var charValue = '0'
                 if (i < stringValue.length) {
